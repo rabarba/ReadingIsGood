df762572b158051c793c08ffd32355d6f1b6e6c6
Uğur Kap <ugurcan.kap@gmail.com>
2021-01-16 20:36:05 +0300

    project is finished

--- ReadingIsGood.API/Program.cs
+++ ReadingIsGood.API/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ReadingIsGood.API
 {
@@ -20,7 +15,10 @@ namespace ReadingIsGood.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    var port = Environment.GetEnvironmentVariable("PORT");
+
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:" + port);
                 });
     }
 }
